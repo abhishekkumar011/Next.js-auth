af5ea5ee601b489eff9553227d5712268185e0d1
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2024-06-23 10:17:46 +0530

    feat: Add email verification endpoint with token validation

---
+++ src/app/api/users/verifyemail/route.ts
@@ -0,0 +1,42 @@
+import User from "@/models/user.model";  
+import { connectDB } from "@/dbConfig/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
+
+connectDB();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { token } = reqBody;
+    console.log("Token" + token)
+
+    if (!token) {
+      return NextResponse.json({ error: "Token is reuired" });
+    }
+
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "Invalid Token" }, { status: 400 });
+    }
+
+    console.log("User details", user);
+
+    user.isVerified = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpiry = undefined;
+
+    await user.save();
+
+    return NextResponse.json({
+      message: "Email verified successfully",
+      success: true,
+    });
+
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
